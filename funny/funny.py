7fb9474d8dc00194ff9c6221a9e8bf9327b23637
Kyle Hendry <kylesh01@gmail.com>
2017-05-02 18:29:39 +0200

    debugging

--- funny/funny.py
+++ funny/funny.py
@@ -61,6 +61,7 @@ class funny:
             soupObject = BeautifulSoup(await response.text(), "html.parser")
             url = response.url
         heading = soupObject.find_all("h2", class_="post-title")
+        print(heading)
         title = heading[0]
         try:
             imgs = soupObject.find_all("img")
