3e38408740582089c77ed142782bd03f6fec1b14
Kyle Hendry <khen0004@student.monash.edu>
2017-05-04 10:02:12 +0200

    first possible working version

--- mudclient/mudclient.py
+++ mudclient/mudclient.py
@@ -2,18 +2,22 @@ import telnetlib
 import threading
 import discord
 import asyncio
+import datetime
 import os # This is required because you will be creating folders/files
 from .utils.dataIO import dataIO  # This is pulled from Twentysix26's utils
 from discord.ext import commands
+
+jsonPath = "data/mudclient/settings.json"
+maxWaitTime = 0.5f
 
 class mudclient:
 
     def __init__(self, bot):
         self.bot = bot
         self.clients = []
-        self.file_path = "data/mudclient/settings.json"
-        self.json_data = dataIO.load_json(self.file_path)
-        self.prefix = self.json_data["interactChar"]
+        self.file_path = jsonPath
+        self.settings = dataIO.load_json(self.file_path)
+        self.prefix = self.settings["interactChar"]
 
     @commands.command(pass_context = True, aliases = ["connect"])
     async def startConnection(self, ctx):
@@ -29,7 +33,7 @@ class mudclient:
                     hasSession = False
 
         if(hasSession != True):
-            clientThread = client(self.bot, user, channel.id,self.json_data["Server"])
+            clientThread = client(self.bot, user, channel.id,self.settings["Server"])
             self.clients.append(clientThread)
             clientThread.start()
             await self.bot.say("```Client Started.\nPlease precede all commands with {}\nClose Session with {}EXIT```".format(self.prefix, self.prefix))
@@ -50,7 +54,7 @@ class mudclient:
 
         self.prefix = prefix
         self.settings['prefix'] = self.prefix
-        dataIO.save_json('data/betterterminal/settings.json', self.settings)
+        dataIO.save_json(jsonPath, self.settings)
         await self.bot.say('`Changed client prefix to {} `'.format(self.prefix))
 
     #get client messages
@@ -77,12 +81,16 @@ class mudclient:
                 if not command:
                     return
 
+                await session._write(command)
+        else:
+            return
+
 
 
 def setup(bot):
     check_folders() # runs the folder check on setup that way it exists before running the cog
     check_files() # runs the check files function to make sure the files you have exists
-    bot.add_cog(funny(bot))
+    bot.add_cog(mudclient(bot))
 
 def check_folders(): # This is how you make your folder that will hold your data for your cog
     if not os.path.exists("data/mudclient"): # Checks if it exists first, if it does, then nothing executes
@@ -93,7 +101,7 @@ def check_folders(): # This is how you make your folder that will hold your data
 def check_files(): # This is how you check if your file exists and let's you create it
     system = {"Server" : {"Name" : "telehack", "IP" : "telehack.com"},
                 "interactChar" : "#"}
-    f = "data/mudclient/settings.json" # f is the path to the file
+    f = jsonPath # f is the path to the file
     if not dataIO.is_valid_json(f): # Checks if file in the specified path exists
         print("Creating default settings.json...") # Prints in console to let the user know we are making this file
         dataIO.save_json(f, system)
@@ -113,13 +121,19 @@ class client():
             print "Bad Connection"
 
     async def start():
+
+        timeSinceLast = 0
+        LastTime = datetime.datetime.now
         while True:
             read = await self.reader.readline()
             if not read:
-                await
-            embed=discord.Embed(title=self.session, description=read)
-            embed.set_author(name=self.user., url='http://stuff.com', icon_url='http://stuff.com')
-            await self.bot.say(embed=embed)
+                timeSinceLast = datetime.datetime.now - LastTime
+            else:
+                LastTime = datetime.datetime.now
+            if timeSinceLast >= maxWaitTime:
+                embed=discord.Embed(title=self.session, description=read)
+                embed.set_author(name=self.user.display_name, icon_url=self.user.avatar_url)
+                await self.bot.say(embed=embed)
 
 
     async def _write(self, message:str):
