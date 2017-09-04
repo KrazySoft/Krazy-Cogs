6c71df87623899f5e4320edc6c0db02bff581a06
Kyle Hendry <khen0004@student.monash.edu>
2017-09-04 11:52:01 +0200

    Testing Fix for Safely Endangered changed formatting

--- funny/funny.py
+++ funny/funny.py
@@ -61,10 +61,10 @@ class funny:
             soupObject = BeautifulSoup(await response.text(), "html.parser")
             url = response.url
         try:
-            imgs = soupObject.find_all("img")
+            comic = soupObject.find(id="comic"}).select("img")
             heading = soupObject.find_all("h2", class_="post-title")
             title = heading[0].contents[0]
-            imgurl = imgs[12]["src"]
+            imgurl = comic["src"]
             em = discord.Embed(title=title, url=url, colour=0x2a2a2b)
             em.set_image(url=imgurl)
             em.set_author(name='safelyendangered.com', icon_url="http://www.safelyendangered.com/wp-content/uploads/2016/01/safely-endangered-comics-1.png")
